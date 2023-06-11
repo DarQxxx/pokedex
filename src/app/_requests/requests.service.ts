3401d228b3be5d699e854aefb52514e1ac0e59f7
Norbert <norbertju@gmail.com>
2023-06-11 12:34:22 +0200

    add gradient to cards

--- src/app/_requests/requests.service.ts
+++ src/app/_requests/requests.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {catchError, concatMap, from, map, Observable, pluck, reduce, tap, throwError} from "rxjs";
+import {catchError, concatMap, forkJoin, from, map, Observable, pluck, reduce, tap, throwError} from "rxjs";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Pokemon} from "../_interfaces/pokemon";
 
@@ -15,19 +15,18 @@ export class RequestsService {
   public getPokemonList(limit: number, offset: number): Observable<Pokemon[]> {
     return this.getRequest(this.apiUrl + `pokemon/?limit=${limit}&offset=${offset}`).pipe(
       pluck('results'),
-      // tap((element: any) => console.log(element)),
       concatMap((elements: {name: string, url: string}[]) =>
-      from(elements).pipe(
-        concatMap((element: { name: string, url: string }) =>
+        forkJoin(elements.map((element: { name: string, url: string }) =>
           this.getRequest(element.url)
-      ))
+        ))
       ),
-      reduce((acc, curr) => acc.concat(curr), []),
+      reduce<any[]>((acc, curr) => acc.concat(curr)),
       map((data: any[]) => {
         return data.map(item => ({
           name: item.name,
           id: item.id,
-          image: item.sprites.front_default,
+          // image: item.sprites.front_default,
+          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + item.id + '.png',
           // image: 'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/' + item.id + '.svg',
           types: item.types,
           stats: item.stats
--- src/app/main/pokedex/pokemon-card/pokemon-card.component.ts
+++ src/app/main/pokedex/pokemon-card/pokemon-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges }  from '@angular/core';
 import {Pokemon} from "../../../_interfaces/pokemon";
-
+import {colors} from "../../../../assets/colors";
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
@@ -9,11 +9,24 @@ import {Pokemon} from "../../../_interfaces/pokemon";
 export class PokemonCardComponent implements OnChanges {
   @Input() pokemonData: Pokemon;
 
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes['pokemonData']) {
-      // console.log(this.pokemonData);
     }
   }
+  getGradientByType(types: any[]): any {
+    // @ts-ignore
+    console.log(colors[types[0].type.name]+ "00")
+    if (types.length - 1)
+      { // @ts-ignore
+        return {'background': 'linear-gradient(to bottom,' + colors[types[0].type.name] + ' 28% ,' + colors[types[1].type.name] +')'};
+      }
+    else
+      { // @ts-ignore
+        return {'background': 'linear-gradient(to top,' + colors[types[0].type.name] + ' 26% ,' + colors[types[0].type.name]+ "88" +')'}
+      }
+  }
+
   getColorByType(type: string): string {
     return 'type--' + type;
   }
